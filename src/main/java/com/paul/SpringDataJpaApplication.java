f99fac3d0e2374714800060275bb7e82ef7b44d7
Ankush <ankushpaulclg2002@gmail.com>
2023-07-15 22:27:27 +0530

    Spring data JPA

---
+++ src/main/java/com/paul/SpringDataJpaApplication.java
@@ -0,0 +1,30 @@
+package com.paul;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student ankush = new Student(
+                    "Purbanka",
+                    "Ghosh",
+                    "purbanka@gmail.com",
+                    21
+            );
+            studentRepository.save(
+                    ankush
+            );
+        };
+    }
+
+}
